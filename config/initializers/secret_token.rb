eb8d21786a2f99e33fc7ea83286db8e056daf3c4
Matthew Loseke <mloseke@gmail.com>
2013-03-16 14:53:22 -0500

    initialize rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebAudioWeekly::Application.config.secret_token = '806cd0d713cc25230ffe46ce6a3d885ff73b45e79bafe62d121be7eebe6de9315cc374597563921c2237db8094f72c59667db1269354e6e78dca7fc5edb637fd'
